bc8b63fcfd740da41e9763f01ae7c9c7410f1abe
ElJuanes15 <eljuanesmercer@gmail.com>
2025-09-25 12:52:29 -0500

    V2

--- app/src/main/java/com/example/recip/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/recip/ui/dashboard/DashboardFragment.kt
@@ -27,7 +27,7 @@ private var _binding: FragmentDashboardBinding? = null
     _binding = FragmentDashboardBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textDashboard
+    val textView: TextView = binding.Profile
     dashboardViewModel.text.observe(viewLifecycleOwner) {
       textView.text = it
     }
---
+++ app/src/main/java/com/example/recip/ui/home/shopping.java
@@ -0,0 +1,6 @@
+package com.example.recip.ui.home;
+
+import androidx.fragment.app.Fragment;
+
+public class shopping extends Fragment {
+}
